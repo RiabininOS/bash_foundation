053155150b960e4816336fab73680a632498d5d9
Riabinin Olexandr Sergiyovych <riabininos@gmail.com>
2026-03-05 15:00:05 +0200

    fixed helix setup for user, but not just root needs test

--- configs/debian/default.sh
+++ configs/debian/default.sh
@@ -5,31 +5,31 @@
 
 # package installation
 apt install -y \
-bash bash-completion shellcheck bashdb fzf
+  bash bash-completion shellcheck shfmt fzf
 
 apt install -y \
-micro hx mc
+  micro hx mc
 
 apt install -y \
-curl wget
+  curl wget
 
 apt install -y \
-git gh
+  git gh
 
 apt install -y \
-sed gawk grep ripgrep
+  sed gawk grep ripgrep
 
 apt install -y \
-python3-full python3-pip
+  python3-full python3-pip
 
 apt install -y \
-npm
+  npm
 
 apt install -y \
-screen tmux mc
+  screen tmux mc
 
 apt install -y \
-kitty
+  kitty
 
 # npm packages
 npm install -g npm@latest
@@ -38,23 +38,25 @@ npm install -g bash-language-server
 
 # git config
 
-echo_info "git un+um system"
-git config --system user.name "Riabinin Olexandr Serhiyovych" && echo "OK sun"
-git config --system user.email "riabininos@gmail.com" && echo "OK sum"
+git config --system user.name "Riabinin Olexandr Serhiyovych"
+git config --system user.email "riabininos@gmail.com"
 
-echo_info "git un+um ug"
-sudo -u "riabininos" git config --global user.name "Riabinin Olexandr Sergiyovych" && echo "OK ugun"
-sudo -u "riabininos" git config --global user.email "riabininos@gmail.com" && echo "OK ugum"
+sudo -u "riabininos" git config --global user.name "Riabinin Olexandr Sergiyovych"
+sudo -u "riabininos" git config --global user.email "riabininos@gmail.com"
 
 git config --system core.editor hx
 sudo -u "riabininos" git config --global core.editor hx
 
 # helix setup
-
+mkdir -p /home/riabininos/.config/
 touch /home/riabininos/.config/helix/config.toml
+chmod 775 /home/riabininos/.config/helix/config.toml
 touch /home/riabininos/.config/helix/languages.toml
+chmod 775 /home/riabininos/.config/helix/languages.toml
 
-cat <<EOF > /home/riabininos/.config/helix/config.toml
+chown -R riabininos /home/riabininos/.config/helix/
+
+cat <<EOF >/home/riabininos/.config/helix/config.toml
 theme = "starlight"
 
 [editor]
@@ -62,9 +64,9 @@ cursorline = true
 color-modes = true
 
 [editor.cursor-shape]
-insert = "bar"
-normal = "block"
-select = "underline"
+insert = "block"
+normal = "underline"
+select = "bar"
 
 [editor.indent-guides]
 render = true
@@ -79,7 +81,7 @@ enable = false
 
 EOF
 
-cat <<EOF > /home/riabininos/.config/helix/languages.toml
+cat <<EOF >/home/riabininos/.config/helix/languages.toml
 [[language]]
 name = "bash"
 file-types = ["config", "sh", "bash", "zsh", ".bash_login", ".bash_logout", ".bash_profile", ".bashrc", ".profile"]
@@ -88,3 +90,6 @@ EOF
 
 hx --grammar fetch
 hx --grammar build
+
+sudo -u "riabininos" hx --gramar fetch
+sudo -u "riabininos" hx --gramar build
