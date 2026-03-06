028c2077fe347b7085e9ad5d7c4ffdcdd155b1fe
Riabinin Olexander Sergiyovych <RiabininOS@gmail.com>
2026-03-06 14:48:35 +0200

    Fix syntax error in powerline.sh check

--- configs/debian/default.sh
+++ configs/debian/default.sh
@@ -100,7 +100,7 @@ sudo -u "riabininos" hx --grammar build
 cat <<EOF >> /home/riabininos/.bashrc
 
 # powerline adding to terminal
-if p -f /usr/share/powerline/bindings/bash/powerline.sh ]; then
+if [ -f /usr/share/powerline/bindings/bash/powerline.sh ]; then
   powerline-daemon -q
   POWERLINE_BASH_CONTINUATION=1
   POWERLINE_BASH_SELECT=1
